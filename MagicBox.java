fe23b14684f66f191384751742d86c6edd29ece9
MukulDP <mukulpatil064@gmail.com>
2026-07-17 22:21:37 +0530

    Pattern Programs for Interviews

---
+++ MagicBox.java
@@ -0,0 +1,14 @@
+class MagicBox{
+    public static void main(String[] args){
+
+        int rows = 5;
+        int cols = 5;
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= cols; j++){
+                System.out.print(i + " " + j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
